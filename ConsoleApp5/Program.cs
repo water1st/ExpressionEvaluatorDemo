e911c56c63d3b12ee4c6c5a2339f1c8e2695ead7
Morris <morris@mogul-tech.com>
2023-03-20 17:28:50 +0800

    优化用例

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -8,25 +8,20 @@ namespace ExpressionEvaluator
     {
         static void Main(string[] args)
         {
-            var bet = new BETEvaluator();
-            var rpn = new RPNEvaluator();
-
-            PrintResultToConsole("RPN", rpn);
-            PrintResultToConsole("BET", bet);
-
-
+            Benchmark.RunAndPrintToConsole();
             BenchmarkRunner.Run<Benchmark>();
         }
 
-        private static void PrintResultToConsole(string name, IEvaluator evaluator)
-        {
-            Console.WriteLine("=============================================================================================");
-            Console.WriteLine($"                                            {name}");
-            Console.WriteLine("=============================================================================================");
-            Console.WriteLine();
 
-            var expressions = new string[]
-            {
+    }
+
+    [ThreadingDiagnoser]
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        // 测试用例
+        private static readonly string[] expressions = new string[]
+        {
                         "4.1+4.1-4.1*4.1/4.1>=100",
                         "1+2*3 == 5",
                         "(1+2) * 3 / 3.14 + 5 - 3",
@@ -52,7 +47,24 @@ namespace ExpressionEvaluator
                         "\"梓杰的的代码又快又省内存\" ## \"代码\"",
                         "\"梓杰的代码\" ## \"垃圾代码\"",
                         "\"梓杰的代码\" !# \"垃圾代码\""
-            };
+        };
+
+
+        private readonly static IEvaluator betEvaluator = new BETEvaluator();
+        private readonly static IEvaluator rpnEvaluator = new RPNEvaluator();
+
+        public static void RunAndPrintToConsole()
+        {
+            PrintResultToConsole("RPN", rpnEvaluator);
+            PrintResultToConsole("BET", betEvaluator);
+        }
+
+        private static void PrintResultToConsole(string name, IEvaluator evaluator)
+        {
+            Console.WriteLine("=============================================================================================");
+            Console.WriteLine($"                                            {name}");
+            Console.WriteLine("=============================================================================================");
+            Console.WriteLine();
 
             foreach (var expression in expressions)
             {
@@ -62,40 +74,7 @@ namespace ExpressionEvaluator
             Console.WriteLine();
             Console.WriteLine("=============================================================================================");
         }
-    }
 
-    [ThreadingDiagnoser]
-    [MemoryDiagnoser]
-    public class Benchmark
-    {
-        // 测试用例
-        private string[] expressions = new string[]
-                {
-                        "4.1+4.1-4.1*4.1/4.1>=100",
-                        "1+2*3 == 5",
-                        "(1+2) * 3 / 3.14 + 5 - 3",
-                        "1 == 2",
-                        "1!=2",
-                        "-11>=-5.6",
-                        "\"bob\" == \"jan\"",
-                        "1<2.2",
-                        "\"2022-11-21\" > \"2019-11-22 23:46:22\"",
-                        "1>=2",
-                        "1<=2",
-                        "[\"bob\",\"jack\"] ## \"bob\"",
-                        "[\"bob\",\"jack\"] ## \"ben\"",
-                        "[\"bob\",\"jack\"] !# \"bob\"",
-                        "[\"bob\",\"jack\"] !# \"jan\"",
-                        "[\"bob\",\"jack\"] !# [\"jan\"]",
-                        "[\"bob\",\"jack\"] ## [\"jan\"]",
-                        "\"梓杰的的代码又快又省内存\" ## \"代码\"",
-                        "\"梓杰的代码\" ## \"垃圾代码\"",
-                        "\"梓杰的代码\" !# \"垃圾代码\""
-                };
-
-
-        private IEvaluator betEvaluator = new BETEvaluator();
-        private IEvaluator rpnEvaluator = new RPNEvaluator();
 
         [Benchmark(OperationsPerInvoke = 10000, Description = "RPN实现")]
         public void RunRPNEvaluator()
